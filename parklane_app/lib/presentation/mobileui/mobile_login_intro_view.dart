ffc5d81f2049780b3d446529d7184471a0fa7e28
suyog2001 <suyog0057@gmail.com>
2022-02-07 11:44:25 +0530

    intro screen fixed

--- parklane_app/lib/presentation/mobileui/mobile_login_intro_view.dart
+++ parklane_app/lib/presentation/mobileui/mobile_login_intro_view.dart
@@ -11,56 +11,54 @@ class MobileLoginIntroView extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.amberAccent.shade100,
-      body: Stack(
+      extendBodyBehindAppBar: true,
+      // resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Parklane',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
+          Padding(
               padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Text(
-                'Parklane',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Let\'s Park',
-                      style: Theme.of(context).textTheme.headline4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Let\'s Park',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Park your vehicle before Present at the place',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushNamed(MobileLoginSignupView.route);
+                    },
+                    child: const Text(
+                      'Let\'s Go',
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Park your vehicle before Present at the place',
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context)
-                            .pushNamed(MobileLoginSignupView.route);
-                      },
-                      child: Text(
-                        'Let\'s Go',
-                      ),
-                      style: ButtonStyle(
-                          minimumSize: MaterialStateProperty.all<Size>(
-                              Size(screenSize.width - 0.1, 50))),
-                    ),
-                  ],
-                )),
-          ),
+                    style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all<Size>(
+                            Size(screenSize.width - 0.1, 50))),
+                  ),
+                ],
+              )),
         ],
       ),
     );
