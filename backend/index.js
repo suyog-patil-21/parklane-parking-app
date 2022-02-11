791a1689fd4c83615b17f07d51bfaae4565de29d
suyog2001 <suyog0057@gmail.com>
2022-02-11 14:06:28 +0530

    nodejs console status

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,21 @@ const port = process.env.PORT || 3000;
 // * Middleware
 app.use(cors());
 // app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+// Console Status
+app.use((req, res, next) => {
+  console.log("Request =>");
+  console.log("\tURL : ", req.url);
+  console.log("\tHeader : ", req.headers);
+  console.log("\tParams : ", req.params);
+  console.log("\tPath :", req.path);
+  console.log("\tBody : ", req.body);
+  console.log("\tRequest IP :", req.ip);
+  console.log("\n");
+
+  next();
+});
 
 // * Auth Routes here
 app.use(authRoutes);
